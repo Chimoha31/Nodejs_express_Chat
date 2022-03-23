c0a31122b321e8c46b6a003a134544fbed6c65e9
Chimoha31 <mokochii1108@gmail.com>
2022-03-23 14:39:29 -0700

    Done

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// serverの作成2-7
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -9,6 +10,19 @@ app.get("/", (req, res) => {
   // __dirnameは今現在のディレクトリーを指す＝server.jsがいるこの階層自体を指す。その階層にhtmlが存在するのでhtmlを呼びたければ以下の書き方となる。
   res.sendFile(__dirname + "/index.html")
 });
+
+// app.get("/")のこのディレクトリーに接続するとuserが接続できるという情報を作成。onは受け取るという意味がある。
+io.on("connection", (socket) => {
+  console.log("User was connected!");
+
+  // index.htmlのemit()で送信したものを受け取っていく処理。onは受け取る。emitは送信。
+  socket.on("chat message", (msg) => {
+    // console.log("Message:" + msg);
+
+    // serverがclientから受け取ったchat messageを更にまたclientに送信するという処理をする。なぜか？全ユーザーが見れるように。
+    io.emit("chat message", msg)
+  })
+})
 
 server.listen(PORT, () => {
   console.log("listening on 3000")
