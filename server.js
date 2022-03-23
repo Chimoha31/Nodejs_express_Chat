fdb1e14e33013c479460e3dc392add06a3c59873
Chimoha31 <mokochii1108@gmail.com>
2022-03-23 13:49:22 -0700

    Changed style file into index.html

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const PORT = 3000;
 app.get("/", (req, res) => {
   // __dirnameは今現在のディレクトリーを指す＝server.jsがいるこの階層自体を指す。その階層にhtmlが存在するのでhtmlを呼びたければ以下の書き方となる。
   res.sendFile(__dirname + "/index.html")
-})
+});
 
 server.listen(PORT, () => {
   console.log("listening on 3000")
-})
+});
